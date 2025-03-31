1fec74e32365be77bfb0e687f98160d7c306876d
ponddevJR <663170010324@rum.ac.th>
2025-03-31 23:19:25 +0700

    config vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,11 @@ import { defineConfig } from "vite"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base:process.env.VITE_BASE_PATH || '/herbs-front-react',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+
 })
